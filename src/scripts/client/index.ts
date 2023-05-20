dc8d01532295578cf91484ee8e0efb87be93b05c
takumus <me@takumus.io>
2023-05-20 20:34:57 +0900

    せい

--- src/scripts/client/index.ts
+++ src/scripts/client/index.ts
@@ -1,4 +1,5 @@
 // import { icon } from "@/scripts/icon";
+// import { debounce } from "throttle-debounce";
 import { getElementsOnPointer } from "@/scripts/client/getElementsOnPointer";
 import { getURLsFromElement } from "@/scripts/client/getURLsFromElement";
 import { Overlay } from "@/scripts/client/overlay";
@@ -23,73 +24,61 @@ import { sendToBackground } from "@/sendToBackground";
       overlay.show(clicledElementRect);
     }
   }, 1000 / 30);
+  async function select(x: number, y: number) {
+    if (!(await sendToBackground("getEnable"))) {
+      return;
+    }
+    const elements = getElementsOnPointer({
+      x,
+      y,
+    });
+    if (elements === undefined) {
+      return;
+    }
+    clickedElement = elements.element;
+    console.log(elements);
+    const elementAlt = elements.element.getAttribute("alt")?.trim();
+    const pageTitle = document.title.trim();
+    const name = (() => {
+      if (elementAlt !== "") {
+        return elementAlt;
+      }
+      if (pageTitle !== "") {
+        return pageTitle;
+      }
+      return "download";
+    })();
+    let urls: string[] = [];
+    Array.from(
+      new Set(
+        [clickedElement, ...elements.elements].map((element) => {
+          return getURLsFromElement(element);
+        })
+      )
+    ).forEach((u) => {
+      u.forEach((url) => {
+        if (url !== undefined) {
+          urls.push(url);
+        }
+      });
+    });
+    urls = Array.from(new Set(urls)).map(
+      (url) => new URL(url, location.href).href
+    );
+    await sendToBackground("orderSave", urls, window.location.origin, {
+      name,
+      note: location.href,
+    });
+  }
   window.addEventListener(
     "mousedown",
-    async (event) => {
+    (event) => {
       if (event.button !== 2) {
         overlay.hide();
         clickedElement = null;
         return;
       }
-      if (!(await sendToBackground("getEnable"))) {
-        return;
-      }
-      const elements = getElementsOnPointer({
-        x: event.clientX,
-        y: event.clientY,
-      });
-      if (elements === undefined) {
-        return;
-      }
-      clickedElement = elements.element;
-      console.log(elements);
-      const elementAlt = elements.element.getAttribute("alt")?.trim();
-      const pageTitle = document.title.trim();
-      const name = (() => {
-        if (elementAlt !== "") {
-          return elementAlt;
-        }
-        if (pageTitle !== "") {
-          return pageTitle;
-        }
-        return "download";
-      })();
-      let urls: string[] = [];
-      Array.from(
-        new Set(
-          [clickedElement, ...elements.elements].map((element) => {
-            return getURLsFromElement(element);
-          })
-        )
-      ).forEach((u) => {
-        u.forEach((url) => {
-          if (url !== undefined) {
-            urls.push(url);
-          }
-        });
-      });
-      urls = Array.from(new Set(urls)).map(
-        (url) => new URL(url, location.href).href
-      );
-      console.log(urls);
-      if (urls.length < 1) {
-        alert("画像が見つかりません");
-        return;
-      }
-      sendToBackground("orderSave", urls, window.location.origin, {
-        name,
-        note: location.href,
-      }).then(() => {
-        // if (ids === undefined) {
-        //   alert("保存失敗");
-        //   return;
-        // }
-        // console.log(ids);
-        // if (ids.length > 0) {
-        //   element.setAttribute("data-imagepetapeta-saved", "true");
-        //   element.style.filter = "invert(100%)";
-        // }
-      });
+      select(event.clientX, event.clientY);
     },
     true
   );
