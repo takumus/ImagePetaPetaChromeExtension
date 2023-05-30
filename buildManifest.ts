78eea541bf1c221cbe2a82e829d632aced11e208
takumus <me@takumus.io>
2023-05-31 08:33:05 +0900

    Update buildManifest.ts

--- buildManifest.ts
+++ buildManifest.ts
@@ -2,6 +2,7 @@ import { manifest } from "./src/manifest";
 import { writeFileSync } from "fs";
 import { resolve } from "path";
 
+console.log("export manifest.json");
 writeFileSync(
   resolve("./dist/manifest.json"),
   JSON.stringify(manifest, undefined, 2)
