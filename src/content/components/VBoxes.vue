7b9f797eafc7f6c93d22124a2ec7cf30e6ac4459
takumus <me@takumus.io>
2024-05-27 05:44:58 +0900

    box分離

--- src/content/components/VBoxes.vue
+++ src/content/components/VBoxes.vue
@@ -1,16 +1,10 @@
 <template>
   <e-boxes-root>
-    <e-box
-      v-for="ipr in imageParserResult"
-      :style="{
-        top: ipr.rect.y + 'px',
-        left: ipr.rect.x + 'px',
-        width: ipr.rect.width + 'px',
-        height: ipr.rect.height + 'px',
-      }"></e-box>
+    <VBox v-for="ipr in imageParserResult" :rect="ipr.rect" />
   </e-boxes-root>
 </template>
 <script setup lang="ts">
+import VBox from "@/content/components/VBox.vue";
 import { ImageParserResult } from "@/content/imageParser";
 
 defineProps<{
@@ -18,22 +12,7 @@ defineProps<{
 }>();
 </script>
 <style lang="scss">
-e-boxes-root > e-box {
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  z-index: 2147483646;
-  margin: 0px;
-  box-shadow:
-    0px 0px 0px 2px #cccccc,
-    0px 0px 0px 2px #cccccc inset,
-    0px 0px 4px 3px rgba(0, 0, 0, 0.4),
-    0px 0px 4px 3px rgba(0, 0, 0, 0.4) inset;
-  border: solid 2px #333333;
-  border-radius: var(--rounded);
-  padding: 0px;
-  width: 100%;
-  height: 100%;
-  pointer-events: none;
+e-boxes-root {
+  display: block;
 }
 </style>
