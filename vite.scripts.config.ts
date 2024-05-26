27e65c65d2884f18d6bfc02defcfba88be091dfb
takumus <me@takumus.io>
2024-05-26 13:39:58 +0900

    upgrade packages

--- vite.scripts.config.ts
+++ vite.scripts.config.ts
@@ -4,24 +4,22 @@ import { defineConfig } from "vite";
 
 // import nodePolyfills from "vite-plugin-node-stdlib-browser";
 
-export default defineConfig(async ({ command }) => {
-  return {
-    build: {
-      minify: false,
-      outDir: resolve("./dist/scripts"),
-      emptyOutDir: true,
-      lib: {
-        entry: {
-          background: resolve("./src/scripts/background/index.ts"),
-          client: resolve("./src/scripts/client/index.ts"),
-        },
-        formats: ["es"],
-        // fileName: "background",
+export default defineConfig({
+  build: {
+    minify: false,
+    outDir: resolve("./dist/scripts"),
+    emptyOutDir: true,
+    lib: {
+      entry: {
+        background: resolve("./src/scripts/background/index.ts"),
+        client: resolve("./src/scripts/client/index.ts"),
       },
+      formats: ["es"],
+      // fileName: "background",
     },
-    resolve: {
-      alias: [{ find: "@", replacement: resolve("./src/") }],
-    },
-    // plugins: [nodePolyfills()],
-  };
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve("./src/") }],
+  },
+  // plugins: [nodePolyfills()],
 });
